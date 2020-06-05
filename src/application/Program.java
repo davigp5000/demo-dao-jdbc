16e858d951d3fda39fc557bf9f9b7adb9ea4e471
DAVI GURGEL <davigp500@gmail.com>
2020-06-04 21:02:55 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,19 @@
 package application;
 
+import java.util.Date;
+
 import model_entities.Departament;
+import model_entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Departament obj = new Departament(1, "Books");
-		System.out.println(obj);
-
+		Seller seller = new Seller(21,"Bob", "bob@gmail.com", new Date(), 3000.0, obj );
+		
+		System.out.println(seller);
+		
 	}
 
 }
