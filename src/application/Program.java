207c29532295acfd50de2d98aac8118829c3a461
DAVI GURGEL <davigp500@gmail.com>
2020-06-08 21:35:03 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -20,15 +21,17 @@ public class Program {
 		Department department = new Department(4, null);
 		List<Seller> list = sellerDao.findByDepartment(department);
 		for (Seller obj : list) {
-
 			System.out.println(obj);
 		}
 		System.out.println("\n=== TEST 3: seller findByALL =====");
 		list = sellerDao.findAll();
 		for (Seller obj : list) {
-
 			System.out.println(obj);
 		}
+		System.out.println("\n=== TEST 4: seller insert =====");
+		Seller newSeller = new Seller(null, "Davi", "davigp500@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = " + newSeller.getId());
 	}
 
 }
